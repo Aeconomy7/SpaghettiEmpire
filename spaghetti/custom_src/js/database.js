b1349e0817ccdd0c8d1c351e6b0de6bd356faa04
Sc00by <aeconomy7@gmail.com>
2018-11-07 13:47:51 -0600

    Update spaghetti/custom_src/js/database.js

--- spaghetti/custom_src/js/database.js
+++ spaghetti/custom_src/js/database.js
@@ -1,6 +1,6 @@
 var app = angular.module('dbApp', []);
 
-app.controller('menuDatabase', function($scope, $http) {
+app.service('menuDatabase', function($scope, $http) {
   $http.get("custom_src/php/menu_database.php")
   .then(function(response) {
     $scope.names = response.data.records;
