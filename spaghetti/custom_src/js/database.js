d42f64eb724824d204c8aa82d3919081ea21f62e
Sc00by <aeconomy7@gmail.com>
2018-11-07 13:51:11 -0600

    Update spaghetti/custom_src/js/database.js

--- spaghetti/custom_src/js/database.js
+++ spaghetti/custom_src/js/database.js
@@ -3,6 +3,7 @@ var app = angular.module('dbApp', []);
 app.service('menuDatabase', function($scope, $http) {
   $http.get("custom_src/php/menu_database.php")
   .then(function(response) {
-    $scope.names = response.data.records;
+    //$scope.names = response.data.records;
+    console.log("fuck");
   });
 });
