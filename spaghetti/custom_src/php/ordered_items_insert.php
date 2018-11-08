5eb3bac2ba7f5b11e72f235c96b1c4ab1c6ae749
Sc00by <aeconomy7@gmail.com>
2018-11-08 15:58:51 -0600

    created all databases and added insert function

---
+++ spaghetti/custom_src/php/ordered_items_insert.php
@@ -0,0 +1,14 @@
+<?php
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Type: application/json; charset=UTF-8");
+
+  $conn = new mysqli("localhost", "root", "F00L\$MUD!", "spaghetti");
+
+  if($conn->connect_errno){
+    echo 'Failed to connect to MySQL: (' . $conn->connect_errno . ') ' . $conn->connect_error;
+  }
+
+  $result = $conn->query('INSERT INTO ordered_items (phone_no, sid, item_name, price, active) VALUES (\'' . $phone_no . '\',\'' . $sid . '\',\'' . $item_name . '\',\'' . $price . '\',\'' . $active . '\')');
+
+  echo($result);
+?>
