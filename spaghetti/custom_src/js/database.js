2895615709e1127f70537d75025b591df542aa34
masonhobbs <masonhobbs@my.unt.edu>
2018-11-08 12:13:31 -0600

    json y

--- spaghetti/custom_src/js/database.js
+++ spaghetti/custom_src/js/database.js
@@ -8,15 +8,19 @@ app.service('menuDatabase', ['$http', function($http) {
       console.log("looking for: " + typeFind);
       $http.get("/spaghetti/custom_src/php/menu_database.php")
         .then(function (response) {
-          var records = JSON.parse(response.data);
-          console.log(records);
-          for(var i = 0; i < records.length; i++) {
+          var result = response.data;
+          console.log(result);
+          console.log(result.message);
+          console.log(result.records);
+          console.log(result.data[0]);
+
+      /*    for(var i = 0; i < records.length; i++) {
             console.log("item:" + records[i]);
             if(records[i].type == typeFind) {
               items.push(records[i]);
             }
           }
-          return items;
+          return items; */
         });
       }
 }]);
