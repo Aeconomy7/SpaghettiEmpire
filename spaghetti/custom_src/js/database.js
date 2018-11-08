f38a04456e44967cb04b6396e1ca2b6a02afc548
masonhobbs <masonhobbs@my.unt.edu>
2018-11-08 12:07:32 -0600

    json parse???

--- spaghetti/custom_src/js/database.js
+++ spaghetti/custom_src/js/database.js
@@ -8,8 +8,8 @@ app.service('menuDatabase', ['$http', function($http) {
       console.log("looking for: " + typeFind);
       $http.get("/spaghetti/custom_src/php/menu_database.php")
         .then(function (response) {
-          var records = response.data;
-          var items;
+          var records = JSON.parse(response.data);
+          console.log(records);
           for(var i = 0; i < records.length; i++) {
             console.log("item:" + records[i]);
             if(records[i].type == typeFind) {
