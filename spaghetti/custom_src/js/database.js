02f0cc0d8eb44e7bc9ab297b405d0d666d8d3fcf
masonhobbs <masonhobbs@my.unt.edu>
2018-11-08 12:15:28 -0600

    perhaps got it?

--- spaghetti/custom_src/js/database.js
+++ spaghetti/custom_src/js/database.js
@@ -9,15 +9,12 @@ app.service('menuDatabase', ['$http', function($http) {
       $http.get("/spaghetti/custom_src/php/menu_database.php")
         .then(function (response) {
           var result = response.data;
-          console.log(result);
-          console.log(result.message);
           console.log(result.records);
-          console.log(result.data[0]);
 
-      /*    for(var i = 0; i < records.length; i++) {
-            console.log("item:" + records[i]);
-            if(records[i].type == typeFind) {
-              items.push(records[i]);
+          for(var i = 0; i < result.records.length; i++) {
+            console.log("item:" + result.records[i]);
+            if(result.records[i].type == typeFind) {
+              items.push(result.records[i]);
             }
           }
           return items; */
