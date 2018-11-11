a83cdf33d7d9b8313be7ea38b4647cbe86ab73a4
Sc00by <aeconomy7@gmail.com>
2018-11-11 13:30:19 -0600

    discount delete / edit added

---
+++ spaghetti/custom_src/php/discount_delete.php
@@ -0,0 +1,17 @@
+<?php
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Type: application/json; charset=UTF-8");
+
+  $conn = new mysqli("localhost", "root", "F00L\$MUD!", "spaghetti");
+
+  if($conn->connect_errno){
+    echo 'Failed to connect to MySQL: (' . $conn->connect_errno . ') ' . $conn->connect_error;
+  }
+
+  $data = json_decode(file_get_contents('php://input'));
+  $name = $data->name;
+
+  $result = $conn->query('DELETE FROM discount WHERE name=\'' . $name . '\'');
+
+  echo($result);
+?>
