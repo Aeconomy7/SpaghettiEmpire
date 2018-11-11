c1a8e0b89c8ca18861e9eb19bd3df5e6774f5e45
Sc00by <aeconomy7@gmail.com>
2018-11-11 13:38:09 -0600

    loyalty update points

---
+++ spaghetti/custom_src/php/loyalty_update_points.php
@@ -0,0 +1,18 @@
+<?php
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Type: application/json; charset=UTF-8");
+
+  $conn = new mysqli("localhost", "root", "F00L\$MUD!", "spaghetti");
+
+  if($conn->connect_errno){
+    echo 'Failed to connect to MySQL: (' . $conn->connect_errno . ') ' . $conn->connect_error;
+  }
+
+  $data = json_decode(file_get_contents('php://input'));
+  $phone_no = $data->phone_no;
+  $pts = $data->pts;
+
+  $result = $conn->query('UPDATE loyalty SET active=' . $pts . ' WHERE phone_no=\'' . $phone_no . '\'');
+
+  echo($result);
+?>
