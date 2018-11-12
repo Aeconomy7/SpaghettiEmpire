eeea9099d6b87c699aa08d60de2b4310a582702d
Sc00by <aeconomy7@gmail.com>
2018-11-12 17:44:50 -0600

    idk lol

--- spaghetti/custom_src/js/customer_data.js
+++ spaghetti/custom_src/js/customer_data.js
@@ -134,13 +134,12 @@ app.service('customerData', function(orderDatabase) {
  //function returns the item with the highest value in order_overall
  function getHighestItemofType(type_f) {
    var highestsum = 0.0;
-   var highestPricedItem = [];//keeps track of the highest priced item
 
    for(var i = 0; i < order_overall.length; i++){
      console.log(order_overall[i]);
      if(order_overall[i].type == type_f && order_overall[i].price >= highestsum){
        console.log('found more expensive item to discount!');
-       highestPricedItem = order_overall[i];
+       var highestPricedItem = order_overall[i];
      }
    }
    console.log('highestPricedItem: ' + highestPricedItem.name);
