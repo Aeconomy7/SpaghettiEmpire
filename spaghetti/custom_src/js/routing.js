21d3d838db630fe52279bf2388978abc14281a3c
Sc00by <aeconomy7@gmail.com>
2018-11-14 11:49:15 -0600

    testing shiz

--- spaghetti/custom_src/js/routing.js
+++ spaghetti/custom_src/js/routing.js
@@ -918,7 +918,8 @@ app.controller('loyaltyRedeemController', function($scope, customerData, discoun
       return;
     }
     var item_to_discount = customerData.getHighestItemofType(type_f);
-    console.log('item_to_discount:' + item_to_discount);
+    console.log('item_to_discount:');
+    console.log(item_to_discount);
     item_to_discount.price = disc_amt;
   }
 });
