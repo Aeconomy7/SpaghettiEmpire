f7e921c9d86692181f1f971b0a6b2831fb536f32
Sc00by <aeconomy7@gmail.com>
2018-11-14 10:08:24 -0600

    added coupons db table and insert / delete / select

---
+++ spaghetti/custom_src/php/coupons_insert.php
@@ -0,0 +1,18 @@
+<?php
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Type: application/json; charset=UTF-8");
+
+  $conn = new mysqli("localhost", "root", "F00L\$MUD!", "spaghetti");
+
+  if($conn->connect_errno){
+    echo 'Failed to connect to MySQL: (' . $conn->connect_errno . ') ' . $conn->connect_error;
+  }
+
+  $data = json_decode(file_get_contents('php://input'));
+  $name = $data->name;
+
+  $result = $conn->query('INSERT INTO coupons (name) VALUES (\'' . $name . '\')');
+
+  echo($result);
+
+?>
---
+++ spaghetti/custom_src/php/coupons_select.php
@@ -0,0 +1,22 @@
+<?php
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Type: application/json; charset=UTF-8");
+
+  $conn = new mysqli("localhost", "root", "F00L\$MUD!", "spaghetti");
+
+  if($conn->connect_errno){
+    echo 'Failed to connect to MySQL: (' . $conn->connect_errno . ') ' . $conn->connect_error;
+  }
+
+  $result = $conn->query('SELECT * FROM coupons');
+
+  $outp = '';
+  while($rs = $result->fetch_array()){
+    if($outp != ""){ $outp .= ","; }
+    $outp .= '{"name":"' . $rs["name"] . '"}';
+  }
+
+  $outp = '{"records":['.$outp.']}';
+
+  echo($outp);
+?>
