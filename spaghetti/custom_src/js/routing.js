4b78be06d01e8f3cf9ffc337973a3e884d6696f4
masonhobbs <masonhobbs@my.unt.edu>
2018-11-13 19:03:06 -0600

    some logs to figure this out

--- spaghetti/custom_src/js/routing.js
+++ spaghetti/custom_src/js/routing.js
@@ -551,6 +551,7 @@ app.controller('managerFinancialController', function($scope, orderDatabase) {
   $scope.orders = [];
   $scope.currentDate = new Date().getDate();
   $scope.currentMonth = new Date().getMonth()+1;
+  $scope.all_orders = [];
   // Date stuff
   var weekLower = $scope.currentDate - 3;
   var weekHigher = $scope.currentDate + 3;
@@ -558,6 +559,7 @@ app.controller('managerFinancialController', function($scope, orderDatabase) {
   // Initialization
   orderDatabase.get_order_history().then(function(response) {
     $scope.all_orders = response;
+    console.log($scope.all_orders);
     $scope.loadTab = "daily";
     console.log($scope.loadTab);
     for(var i = 0; i < $scope.all_orders.length; i++) {
@@ -571,6 +573,8 @@ app.controller('managerFinancialController', function($scope, orderDatabase) {
   $scope.changeTab = function(tabName) {
     $scope.loadTab = tabName;
     console.log($scope.loadTab);
+    console.log($scope.all_orders);
+
     $scope.orders = [];
     for(var i = 0; i < $scope.all_orders.length; i++) {
       // CURRENT DAY
