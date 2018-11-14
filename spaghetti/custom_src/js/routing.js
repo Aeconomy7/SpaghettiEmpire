01747e262d7c52e487707ca42751df90326a9455
Sc00by <aeconomy7@gmail.com>
2018-11-14 13:46:35 -0600

    frickin frick

--- spaghetti/custom_src/js/routing.js
+++ spaghetti/custom_src/js/routing.js
@@ -910,7 +910,7 @@ app.controller('loyaltyProfileController', function($scope, customerData, loyalt
 
 });
 
-app.controller('loyaltyRedeemController', function($scope, $route, customerData, orderDatabase, discountDatabase) {
+app.controller('loyaltyRedeemController', function($scope, $route, customerData, orderDatabase, discountDatabase, loyaltyDatabase) {
   $scope.pageName = "Redeem Loyalty Points";
 
   discountDatabase.getRewards("appetizer").then(function(response) {
